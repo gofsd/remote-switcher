20649b3a800b52de7560cc95be0811bc08889c4b
gofsd1 <madi.nickname@gmail.com>
2024-08-07 00:50:59 +0300

    fix

--- lib/HttpSwitcher/HttpSwitcher.cpp
+++ lib/HttpSwitcher/HttpSwitcher.cpp
@@ -40,8 +40,8 @@ const char *ssid = "esp8266";
 const char *password = "Password1";
 const int kNetworkDelay = 1000;
 const int kNetworkTimeout = 30 * 1000;
-char result[]; // array to hold the result.
-JsonDocument doc(1024);
+char result[1024]; // array to hold the result.
+StaticJsonDocument<1024> doc;
 DynamicJsonDocument updDoc(512);
 DynamicJsonDocument updSettings(512);
 bool shouldReboot = false;
@@ -49,8 +49,38 @@ bool shouldReboot = false;
 const char *PARAM_MESSAGE = "message";
 const char *PIN = "pin";
 const char *VALUE = "value";
+char html_buffer[1024];
+int max_len = sizeof html_buffer;
+const char doctype[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> %s </title>
+</head>
+
+<body>
+%s
+</body>
+
+</html>)rawliteral";
+
+const char setting[] PROGMEM =  R"rawliteral(
+    <form action="/setting">
+      <label for="fname">First name:</label><br>
+      <input type="text" id="fname" name="fname" value="John" disabled><br>
+      <label for="lname">Last name:</label><br>
+      <input type="text" id="lname" name="lname" value="Doe">
+    </form>
+)rawliteral";
+    char* s = "geeksforgeeks";
+
+
 
 boolean toggle = false;
+
+StaticJsonDocument<1024> jsonData;
 void notFound(AsyncWebServerRequest *request)
 {
   request->send(404, "text/plain", "Not found");
@@ -130,7 +160,10 @@ void start()
     request->send(200, "text/plain", "test"); });
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(200, "text/plain", "Hello, world"); });
+            {
+              snprintf(html_buffer, max_len, doctype, s, setting);
+              Serial.println(html_buffer);
+              request->send(200, "text/html", html_buffer); });
 
   server.on("/pin", HTTP_GET, [](AsyncWebServerRequest *request)
             {
@@ -229,6 +262,129 @@ void start()
   json += "]";
   request->send(200, "application/json", json);
   json = String(); });
+
+    // Create (POST): Set pin state
+  server.on("/setPin", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+    StaticJsonDocument<512> doc;
+    DeserializationError error = deserializeJson(doc, data);
+
+    if (error) {
+      request->send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
+      return;
+    }
+
+    int pin = doc["pin"];
+    String mode = doc["mode"];
+    int value = doc["value"];
+
+    if (mode == "analogInput") {
+      pinMode(pin, INPUT);
+      int analogValue = analogRead(pin);
+      jsonData["pins"][String(pin)]["mode"] = "analogInput";
+      jsonData["pins"][String(pin)]["value"] = analogValue;
+    } else if (mode == "digitalInput") {
+      pinMode(pin, INPUT);
+      int digitalValue = digitalRead(pin);
+      jsonData["pins"][String(pin)]["mode"] = "digitalInput";
+      jsonData["pins"][String(pin)]["value"] = digitalValue;
+    } else if (mode == "digitalOutput") {
+      pinMode(pin, OUTPUT);
+      digitalWrite(pin, value);
+      jsonData["pins"][String(pin)]["mode"] = "digitalOutput";
+      jsonData["pins"][String(pin)]["value"] = value;
+    } else if (mode == "pwmOutput") {
+      pinMode(pin, OUTPUT);
+      analogWrite(pin, value);
+      jsonData["pins"][String(pin)]["mode"] = "pwmOutput";
+      jsonData["pins"][String(pin)]["value"] = value;
+    } else {
+      request->send(400, "application/json", "{\"error\":\"Invalid mode\"}");
+      return;
+    }
+
+    request->send(200, "application/json", "{\"status\":\"pin set\"}");
+  });
+
+  // Read (GET): Get current pin states
+  server.on("/getPins", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String jsonStr;
+    serializeJson(jsonData, jsonStr);
+    request->send(200, "application/json", jsonStr);
+  });
+
+  // Update (PUT): Update pin state
+  server.on("/updatePin", HTTP_PUT, [](AsyncWebServerRequest *request) {}, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+    StaticJsonDocument<512> doc;
+    DeserializationError error = deserializeJson(doc, data);
+
+    if (error) {
+      request->send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
+      return;
+    }
+
+    int pin = doc["pin"];
+    String mode = doc["mode"];
+    int value = doc["value"];
+
+    if (mode == "analogInput") {
+      pinMode(pin, INPUT);
+      int analogValue = analogRead(pin);
+      jsonData["pins"][String(pin)]["mode"] = "analogInput";
+      jsonData["pins"][String(pin)]["value"] = analogValue;
+      Serial.print(analogValue);
+    } else if (mode == "digitalInput") {
+      pinMode(pin, INPUT);
+      int digitalValue = digitalRead(pin);
+      jsonData["pins"][String(pin)]["mode"] = "digitalInput";
+      jsonData["pins"][String(pin)]["value"] = digitalValue;
+    } else if (mode == "digitalOutput") {
+      pinMode(pin, OUTPUT);
+      digitalWrite(pin, value);
+      jsonData["pins"][String(pin)]["mode"] = "digitalOutput";
+      jsonData["pins"][String(pin)]["value"] = value;
+    } else if (mode == "pwmOutput") {
+      pinMode(pin, OUTPUT);
+      analogWrite(pin, value);
+      jsonData["pins"][String(pin)]["mode"] = "pwmOutput";
+      jsonData["pins"][String(pin)]["value"] = value;
+    } else {
+      request->send(400, "application/json", "{\"error\":\"Invalid mode\"}");
+      return;
+    }
+
+    request->send(200, "application/json", "{\"status\":\"pin updated\"}");
+  });
+
+  // Delete (DELETE): Reset pin state
+  server.on("/resetPin", HTTP_DELETE, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("pin", true)) {
+      int pin = request->getParam("pin", true)->value().toInt();
+      jsonData["pins"].remove(String(pin));
+      pinMode(pin, INPUT);  // Reset pin to default state
+      request->send(200, "application/json", "{\"status\":\"pin reset\"}");
+    } else {
+      request->send(400, "application/json", "{\"error\":\"No pin specified\"}");
+    }
+  });
+
+  // Get all pins state
+  server.on("/getAllPins", HTTP_GET, [](AsyncWebServerRequest *request) {
+    for (JsonPair kv : jsonData["pins"].as<JsonObject>()) {
+      const char* key = kv.key().c_str();
+      int pin = atoi(key);
+      const char* mode = kv.value()["mode"];
+
+      if (strcmp(mode, "analogInput") == 0) {
+        jsonData["pins"][key]["value"] = analogRead(pin);
+      } else if (strcmp(mode, "digitalInput") == 0) {
+        jsonData["pins"][key]["value"] = digitalRead(pin);
+      }
+    }
+
+    String jsonStr;
+    serializeJson(jsonData, jsonStr);
+    request->send(200, "application/json", jsonStr);
+  });
 
   server.onNotFound(notFound);
 
